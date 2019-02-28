d80e3d156fd57301b1af4020ae2ba7c61d8469f6
samy30 <samibelaid30@gmail.com>
2019-02-28 17:05:05 +0100

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Dashboard} from "./components/Dashboard";
+import {LandingPage} from "./components/LandingPage";
+import { BrowserRouter, Route} from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={LandingPage}/>
+        <Route path="/dashboard" component={Dashboard}/>
       </div>
+    </BrowserRouter>
     );
   }
 }
