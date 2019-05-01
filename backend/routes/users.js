87d2bcfc22cf6cae69902818eca441d16a94815c
ali-98 <slamaali@outlook.fr>
2019-05-02 00:14:56 +0100

    added attributes to user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -23,10 +23,9 @@ router.post('/',wrapper(async (req,res)=>{
     let user=await User.findOne({email: req.body.email});
     if(user) return res.status(400).send('User already Registred');
 
-    user=new User({email:req.body.email});
+    user=new User({email:req.body.email,fullName:req.body.fullName});
     user.setPassword(req.body.password);
     // logger.info(user);
-
     await user.save();
     res.send(user);
 }));
