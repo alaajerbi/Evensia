ee289228ce13b1828957f574d49f10607a3f0fdc
Alaa Jerbi <mralaajerbi@gmail.com>
2019-05-03 01:36:34 +0100

    Fix login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,19 +7,30 @@ const logger=require('../logger');
 
 router.post('/',async (req,res)=>{
     const {err}=validate(req.body);
-    if(err) return res.status(400).send(err.details[0].message);
+    if(err) return res.json({
+        status: 'error',
+        error: err.details[0].message
+    })
 
     const user=await User.findOne({email:req.body.email});
-    if(!user) return res.status(400).send('Invalid email or password');
+    if(!user) return res.json({
+        status: 'error',
+        error: 'User not found'
+    });
 
     const validPassword=user.validatePassword(req.body.password);
-    if(!validPassword) return res.status(400).send('Invalid email or password');
+    if(!validPassword) return res.json({
+        status: 'error',
+        error: 'Invalid password'
+    });
 
     const token=user.generateAuthToken();
     res.header({
         'Access-Control-Expose-Headers': 'token',
         'token':token,
-     }).send(true);
+     }).json({
+         status: 'success'
+     });
 
 });
 
