292c6aa77195c501f26f7838956b263c63bcd3b7
Alaa Jerbi <mralaajerbi@gmail.com>
2019-05-03 05:54:38 +0100

    Fix js imports

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import {BrowserRouter as Router, Route}
 import AuthPage from "../components/AuthPage";
 import Sidebar from "../components/Sidebar";
 import Nav from '../components/Nav';
@@ -8,6 +8,7 @@ import Footer from "../components/Footer";
 class Dashboard extends Component {
   render() {
     return (
+      <Router>
       <AuthPage>
         <div class="wrapper ">
         <Sidebar />   
@@ -15,12 +16,19 @@ class Dashboard extends Component {
             {/* Navbar */}
             <Nav />
             <div class="content">
-              
+            <Route path="/guests" component={Guests} />
+            <Route path="/tasks" component={Tasks} />
+            <Route exact path="/event/:eventId/edit" component={EditEvent} />
+            <Route exact path="/event/:eventId/guests" component={Guests} />
+            <Route exact path="/event/:eventId" component={Event} />
+            <Route exact path="/events" component={Events} />
+            <Route path="/events/create" component={AddEvent} />
             </div>
            <Footer />
           </div>
         </div>
       </AuthPage>
+      </Router>
     );
   }
 }
